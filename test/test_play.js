d54d34cc65e70eef3e426082ccea28318504d563
Anthony Brown <anthony@found-it.net>
2017-03-05 14:52:40 +0000

    initial creation

---
+++ test/test_play.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "session": {
+    "new": true,
+    "sessionId": "session1234",
+    "attributes": {},
+    "user": {
+      "userId": null
+    },
+    "application": {
+      "applicationId": "amzn1.ask.skill.749c5f66-1293-4ea9-85c1-5cda8f9215a5"
+    }
+  },
+  "version": "1.0",
+  "request": {
+    "intent": {
+      "slots": {
+        "Room": {
+          "name": "Room",
+          "value": "bedroom"
+        }
+      },
+      "name": "ResumeIntent"
+    },
+    "type": "IntentRequest",
+    "requestId": "request5678"
+  }
+}
